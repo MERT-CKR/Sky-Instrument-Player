a5b151d66598e9f7f193931c646aa14d4be046f0
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2025-09-25 12:21:46 +0300

    Add files via upload

--- modules/player_core.py
+++ modules/player_core.py
@@ -1,15 +1,34 @@
 from modules.utils import *
 from modules.utils import load_translations
 _ = load_translations()
-from keyboard import press, is_pressed, press_and_release
+from keyboard import is_pressed, press_and_release
+import keyboard
 
 
 sheet_keys = ["1Key0","1Key1","1Key2","1Key3","1Key4","1Key5","1Key6","1Key7","1Key8","1Key9","1Key10","1Key11","1Key12","1Key13","1Key14"][::-1]
 
+
+def while_speed_test():
+    x = 0
+    count = 0
+    start = time.time()
+    while time.time() - start < 1:
+        x = 1
+        count += 1
+    print("1 saniyede:", count)
+
+
+
 def playMusic(sheet, keybinds):
+    print("benchmark test is running")
+    while_speed_test()
+    while_speed_test()
+    while_speed_test()
+
     target = select_window()
     timer(1)
-
+    t1 = time.time()
+    counter = 0
     for line in sheet:
         key = line["key"]
         time1 = line["time"]#1 to avoid confusion with the library
@@ -20,14 +39,15 @@ def playMusic(sheet, keybinds):
 
         
         print(f"Time: {time1} Key {key.capitalize()}")
+       
         
-        counter = 0
-        current_time = timer()
+        current_time = timer() 
         while current_time < time1: # Wait for correct time
             current_time = timer()
             counter +=1
-            print(counter)
-            if counter%200 == 0:
+            time.sleep(0.0005)
+            
+            if counter%150 == 0:
                 print("\n")
 
         
@@ -39,6 +59,10 @@ def playMusic(sheet, keybinds):
                 print(_("focus_lost"))
                 break
         
-        press_and_release(key)
+        keyboard.send(key)
         
-        # release(key)
+
+    t2 = time.time()
+    playtime = round(t2 - t1, 1)
+    # print_yellow(_("sheet_type_note"))
+    print(_("playback_duration").replace("*", str(playtime)))
